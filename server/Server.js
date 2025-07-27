65e1330fc0ffae50ef651212cba16b5be7984784
khushi11-19 <sharmakhushi0908@gmail.com>
2025-07-27 12:15:37 +0530

    new change

--- server/Server.js
+++ server/Server.js
@@ -7,11 +7,10 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Connect MongoDB
 try {
   mongoose.connect(process.env.MONGODB_URI);
   console.log("MongoDb connected...");
@@ -19,11 +18,11 @@ try {
   console.log(error);
 }
 
-// ✅ Add this to use your auth routes
+
 const authRoutes = require("./routes/authRoutes");
 app.use("/api", authRoutes);
 
-// Optional: test route
+
 app.get("/", (req, res) => {
   res.send("API working ✅");
 });
